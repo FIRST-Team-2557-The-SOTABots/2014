92111e8bcd0c344200388aaab3bd5f76ba43c98d
VaneRaklan <antoniowii@yahoo.com>
2014-03-10 13:14:36 -0700

    Update RobotMap.java
    I added each of the parts and labeled the corresponding ports.

--- 2557_2014/src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ 2557_2014/src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -7,13 +7,24 @@ package edu.wpi.first.wpilibj.templates;
  * floating around.
  */
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static final int leftMotor = 1;
-    // public static final int rightMotor = 2;
+    public static final int leftDriveMotor = 1;
+    public static final int rightDriveMmotor = 2;
+    public static final int intakeMotor = 3;
+    public static final int winchMotor = 4;
+    public static final int arduino_1 = 1;
+    public static final int arduino_2 = 2;
+    public static final int limSwitchPort = 3;
     
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static final int rangefinderPort = 1;
-    // public static final int rangefinderModule = 1;
+    public static final int intakeUp = 1;
+    public static final int intakeDown = 2;
+    public static final int unshift = 3;
+    public static final int shift = 4;
+    public static final int unlatched = 5;
+    public static final int latched = 6;
+    
+    public static final int leftEncoder_a = 5;
+    public static final int leftEncoder_b = 6;
+    public static final int rightEncoder_a = 7;
+    public static final int rightEncoder_b = 8;
+    
 }
