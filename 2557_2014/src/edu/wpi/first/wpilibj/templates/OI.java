c44fbd0cd17c45d2e3a6b42b366b45f9d0a1ca43
Preston Holmgren <guitarpreston>
2014-03-10 16:36:10 -0700

    I defined every Button on the Joystick. We will probably delete some buttons later. And i commented on all of them.

--- 2557_2014/src/edu/wpi/first/wpilibj/templates/OI.java
+++ 2557_2014/src/edu/wpi/first/wpilibj/templates/OI.java
@@ -17,30 +17,30 @@ public class OI {
     static Joystick leftJoy = new Joystick(RobotMap.leftJoyP); //Defines left joystick.
     static Joystick rightJoy = new Joystick(RobotMap.rightJoyP); //Defines right joystick.
     //Defines the left joystick buttons.
-    Button buttonL1 = new JoystickButton(leftJoy, 1);
-    Button buttonL2 = new JoystickButton(leftJoy, 2);
-    Button buttonL3 = new JoystickButton(leftJoy, 3);
-    Button buttonL4 = new JoystickButton(leftJoy, 4);
-    Button buttonL5 = new JoystickButton(leftJoy, 5);
-    Button buttonL6 = new JoystickButton(leftJoy, 6);
-    Button buttonL7 = new JoystickButton(leftJoy, 7);
-    Button buttonL8 = new JoystickButton(leftJoy, 8);
-    Button buttonL9 = new JoystickButton(leftJoy, 9);
-    Button buttonL10 = new JoystickButton(leftJoy, 10);
-    Button buttonL11 = new JoystickButton(leftJoy, 11);
-    Button buttonL12 = new JoystickButton(leftJoy, 12);
-    Button buttonR1 = new JoystickButton(rightJoy, 1);
-    Button buttonR2 = new JoystickButton(rightJoy, 2);
-    Button buttonR3 = new JoystickButton(rightJoy, 3);
-    Button buttonR4 = new JoystickButton(rightJoy, 4);
-    Button buttonR5 = new JoystickButton(rightJoy, 5);
-    Button buttonR6 = new JoystickButton(rightJoy, 6);
-    Button buttonR7 = new JoystickButton(rightJoy, 7);
-    Button buttonR8 = new JoystickButton(rightJoy, 8);
-    Button buttonR9 = new JoystickButton(rightJoy, 9);
-    Button buttonR10 = new JoystickButton(rightJoy, 10);
-    Button buttonR11 = new JoystickButton(rightJoy, 11);
-    Button buttonR12 = new JoystickButton(rightJoy, 12);
+    Button buttonL1 = new JoystickButton(leftJoy, 1); //Defines left joystick button 1
+    Button buttonL2 = new JoystickButton(leftJoy, 2); //Defines left joystick button 2
+    Button buttonL3 = new JoystickButton(leftJoy, 3); //Defines left joystick button 3
+    Button buttonL4 = new JoystickButton(leftJoy, 4); //Defines left joystick button 4
+    Button buttonL5 = new JoystickButton(leftJoy, 5); //Defines left joystick button 5
+    Button buttonL6 = new JoystickButton(leftJoy, 6); //Defines left joystick button 6
+    Button buttonL7 = new JoystickButton(leftJoy, 7); //Defines left joystick button 7
+    Button buttonL8 = new JoystickButton(leftJoy, 8); //Defines left joystick button 8
+    Button buttonL9 = new JoystickButton(leftJoy, 9); //Defines left joystick button 9
+    Button buttonL10 = new JoystickButton(leftJoy, 10); //Defines left joystick button 10
+    Button buttonL11 = new JoystickButton(leftJoy, 11); //Defines left joystick button 11
+    Button buttonL12 = new JoystickButton(leftJoy, 12); //Defines left joystick button 12
+    Button buttonR1 = new JoystickButton(rightJoy, 1); //Defines right joystick button 1
+    Button buttonR2 = new JoystickButton(rightJoy, 2); //Defines right joystick button 2
+    Button buttonR3 = new JoystickButton(rightJoy, 3); //Defines right joystick button 3
+    Button buttonR4 = new JoystickButton(rightJoy, 4); //Defines right joystick button 4
+    Button buttonR5 = new JoystickButton(rightJoy, 5); //Defines right joystick button 5
+    Button buttonR6 = new JoystickButton(rightJoy, 6); //Defines right joystick button 6
+    Button buttonR7 = new JoystickButton(rightJoy, 7); //Defines right joystick button 7
+    Button buttonR8 = new JoystickButton(rightJoy, 8); //Defines right joystick button 8
+    Button buttonR9 = new JoystickButton(rightJoy, 9); //Defines right joystick button 9
+    Button buttonR10 = new JoystickButton(rightJoy, 10); //Defines right joystick button 10
+    Button buttonR11 = new JoystickButton(rightJoy, 11); //Defines right joystick button 11
+    Button buttonR12 = new JoystickButton(rightJoy, 12); //Defines right joystick button 12
     
     
     //// CREATING BUTTONS
