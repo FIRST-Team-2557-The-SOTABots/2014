8885ee38b6c3a528ee0e7dc4fe8b099fb667565f
Avery Reich-Norris <>
2014-03-10 17:48:11 -0700

    I changed some of the to the variable names and made compressor public.

--- 2557_2014/src/edu/wpi/first/wpilibj/templates/RobotParts.java
+++ 2557_2014/src/edu/wpi/first/wpilibj/templates/RobotParts.java
@@ -31,24 +31,24 @@ public class RobotParts {
     public static Encoder leftDriveEn = new Encoder(RobotMap.leftEnAP,RobotMap.leftEnBP); //Defines left drive encoders.
     public static Encoder rightDriveEn = new Encoder(RobotMap.rightEnAP,RobotMap.rightEnBP); //Defines right drive encoders.
     //Defines Compressor.
-    static Compressor compress = new Compressor(RobotMap.compressorAP,RobotMap.compressorBP);//Defines the compressor as compressor.
+    public static Compressor compressor = new Compressor(RobotMap.compressorAP,RobotMap.compressorBP);//Defines the compressor as compressor.
     //Defines solenoids of the pneumatics by their jobs.
-    public static Solenoid intakeUp = new Solenoid(RobotMap.intakeUpP); //Defines intake arm moving up.
-    public static Solenoid intakeDown = new Solenoid(RobotMap.intakeDownP); //Defines intake arm moving down.
-    public static Solenoid unShift = new Solenoid(RobotMap.unshiftP); //Defines unshift gears on supershift.
-    public static Solenoid shift = new Solenoid(RobotMap.shiftP); //Defines shift gears on supershifter.
-    public static Solenoid fireCata = new Solenoid(RobotMap.unlatchedP); //Defines unlatching catapult arm.
-    public static Solenoid latchCata = new Solenoid(RobotMap.latchedP); //Defines latching catapult arm.
+    public static Solenoid intakeUpSol = new Solenoid(RobotMap.intakeUpP); //Defines intake arm moving up.
+    public static Solenoid intakeDownSol = new Solenoid(RobotMap.intakeDownP); //Defines intake arm moving down.
+    public static Solenoid unShiftSol = new Solenoid(RobotMap.unshiftP); //Defines unshift gears on supershift.
+    public static Solenoid shiftSol = new Solenoid(RobotMap.shiftP); //Defines shift gears on supershifter.
+    public static Solenoid fireCataSol = new Solenoid(RobotMap.unlatchedP); //Defines unlatching catapult arm.
+    public static Solenoid latchCataSol = new Solenoid(RobotMap.latchedP); //Defines latching catapult arm.
     //Defines digital inputs by jobs.
-    static DigitalInput cataLim = new DigitalInput(RobotMap.cataLimP); //Defines sensor that detects catapult limit.
+    static DigitalInput cataLimIn = new DigitalInput(RobotMap.cataLimP); //Defines sensor that detects catapult limit.
     //Defines digital ouputs based on jobs.
-    static DigitalOutput ledFlash1 = new DigitalOutput(RobotMap.ledFlash1P); //Defines led "place type of flash here".
-    static DigitalOutput ledFlash2 = new DigitalOutput(RobotMap.ledFlash2P); //Defines led "place type of flash here".
-    static DigitalOutput ledFlash3 = new DigitalOutput(RobotMap.ledFlash3P); //Defines led "place type of flash here".
-    static DigitalOutput ledFlash4 = new DigitalOutput(RobotMap.ledFlash4P); //Defines led "place type of flash here".
-    static DigitalOutput ledFlash5 = new DigitalOutput(RobotMap.ledFlash5P); //Defines led "place type of flash here".
+    static DigitalOutput ledYlwOut = new DigitalOutput(RobotMap.ledFlash1P); //Defines led Yellow.
+    static DigitalOutput ledBluOut = new DigitalOutput(RobotMap.ledFlash2P); //Defines led Blue.
+    static DigitalOutput ledRedOut = new DigitalOutput(RobotMap.ledFlash3P); //Defines led Red.
+    static DigitalOutput ledMuricaOut = new DigitalOutput(RobotMap.ledFlash4P); //Defines led Murica.
+    static DigitalOutput ledGroovyOut = new DigitalOutput(RobotMap.ledFlash5P); //Defines led Groovy.
     //Defines SmartDashboard.
-    static SmartDashboard dash = new SmartDashboard(); //Defines a SmartDashboard as dash for ease of reference.
+    static SmartDashboard driveStation = new SmartDashboard(); //Defines a SmartDashboard as dash for ease of reference.
     //Defines Timer.
     public static Timer timer = new Timer(); //Defines a Timer as timer for ease of reference.
 }
