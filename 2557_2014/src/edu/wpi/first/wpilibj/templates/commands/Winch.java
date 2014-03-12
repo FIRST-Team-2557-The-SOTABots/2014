fd7abc8edec21ad5c180e439d96d18810e67243c
Avery Reich-Norris <>
2014-03-12 13:07:44 -0700

    I created the winch class that takes some info into account as it does its stuff.

---
+++ 2557_2014/src/edu/wpi/first/wpilibj/templates/commands/Winch.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.templates.RobotParts;
+/**
+ *
+ * @author 4053
+ */
+public class Winch extends CommandBase {
+    
+    Command latch = new Latch(true);
+    double winch = 0;
+    public Winch(double winch) {
+        this.winch = winch;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        RobotParts.winchM.set(winch);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        if(RobotParts.winchEn.get() >= 510 && RobotParts.cataLimIn.get()){
+            RobotParts.winchM.stopMotor();
+            latch.start();
+        }
+        else{
+            RobotParts.winchM.set(winch);
+        }
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return RobotParts.cataLimIn.get();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        RobotParts.winchM.stopMotor();
+    }
+}
